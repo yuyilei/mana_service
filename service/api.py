6e803c055e82359685256ca652934e82d1afa4d7
Humbertzhang <504490160@qq.com>
2018-01-31 11:02:49 +0800

    UPDATE site info

--- service/api.py
+++ service/api.py
@@ -81,40 +81,52 @@ apartment_list = [
 
 site_list = [
     {
+        "site":"网上一站式服务门户",
+        "url":"http://one.ccnu.edu.cn"
+    },
+    {
         "site": "信息门户",
         "url": "http://portal.ccnu.edu.cn/"
-    },
-    {
-        "site": "学生信息服务平台",
-        "url": "http://xssw.ccnu.edu.cn/zfca/login"
-    },
-    {
-        "site": "微课程平台",
-        "url": "http://wk.ccnu.edu.cn/hsd-web/index.do"
     },
     {
         "site": "素质课程管理平台",
         "url": "http://122.204.187.9/jwglxt/xtgl/dl_loginForward.html"
     },
     {
-        "site": "教务处",
-        "url": "http://jwc.ccnu.edu.cn/"
+        "site":"图书馆首页",
+        "url":"http://lib.ccnu.edu.cn"
     },
     {
-        "site": "网上自主缴费平台",
-        "url": "http://218.199.196.90/"
+        "site": "微课程平台",
+        "url": "http://wk.ccnu.edu.cn/hsd-web/index.do"
+    },
+    {
+        "site": "全国大学四六级",
+        "url": "http://www.cet.edu.cn"
+    },
+    {
+        "site": "尔雅课",
+        "url": "http://ccnu.benke.chaoxing.com/"
+    },
+    {
+        "site": "教务处",
+        "url": "http://jwc.ccnu.edu.cn/"
     },
     {
         "site": "校医院",
         "url": "http://hosp.ccnu.edu.cn/"
     },
     {
+        "site": "网上自主缴费平台",
+        "url": "http://218.199.196.90/"
+    },
+    {
         "site": "图书馆研习室预约",
         "url": "http://202.114.34.12:8088/reserveSystem/readerIndex.html"
     },
     {
-        "site": "尔雅课",
-        "url": "http://ccnu.benke.chaoxing.com/"
+        "site": "志愿者管理系统",
+        "url": "http://zyz.ccnu.edu.cn"
     },
 ]
 
